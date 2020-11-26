25174ffdb02a309bb14e37ca4c971b22e70dcf35
hongbingkun <hongbingkun@qq.com>
2020-11-26 09:29:49 +0800

    fix:用户登录模拟数据格式修改

--- src/mock/services/auth.js
+++ src/mock/services/auth.js
@@ -6,23 +6,7 @@ const login = () => {
   Cookies.set('tokenid', 'MQ%3D%3D')
   Cookies.set('logintime', '1606266713')
   Cookies.set('keystr', 'd5745e54ad1417dd31d1bc63a7d47353')
-  return builder({
-    'id': Mock.mock('@guid'),
-    'name': Mock.mock('@name'),
-    'username': 'admin',
-    'password': '',
-    'avatar': 'https://gw.alipayobjects.com/zos/rmsportal/jZUIxmJycoymBprLOUbT.png',
-    'status': 1,
-    'telephone': '',
-    'lastLoginIp': '27.154.74.117',
-    'lastLoginTime': 1534837621348,
-    'creatorId': 'admin',
-    'createTime': 1497160610259,
-    'deleted': 0,
-    'roleId': 'admin',
-    'lang': 'zh-CN',
-    'token': '4291d7da9005377ec9aec4a71ea837f'
-  }, '', 200, { 'Custom-Header': Mock.mock('@guid') })
+  return builder(Mock.mock('@name'), '', 200, { 'Custom-Header': Mock.mock('@guid') })
 }
 
 const logout = () => {
